f7bcc18345f9200a09fd773a16c2c73314ceffbe
kimberlly florez <kimberlly.florez@gmail.com>
2023-05-13 21:40:52 +1000

    feat: Create a prototype of design system

---
+++ lib/design_system/spacing.dart
@@ -0,0 +1,19 @@
+class Spacing {
+  Spacing();
+
+  static const double none = 0.0;
+
+  static const double xxxs = 4;
+
+  static const double xxs = 8;
+
+  static const double xl = 48;
+
+  static const double xs = 16;
+
+  static const double m = 26;
+
+  static const double l = 32;
+
+  static const double xxl = 64.0;
+}
