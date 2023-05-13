70c83aaf96facde6273b9078bb2199022c7d715b
kimberlly florez <kimberlly.florez@gmail.com>
2023-05-13 21:40:52 +1000

    feat: Create a prototype of design system

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:caffeio/di/di.dart';
 import 'package:caffeio/domain/use_cases/brewing_methods/fetch_brewing_methods_uc.dart';
 import 'package:caffeio/domain/use_cases/brewing_methods/get_brewing_methods_uc.dart';
 import 'package:caffeio/entities/brewing_methods/brewing_method.dart';
+import 'package:caffeio/design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -15,17 +16,23 @@ class HomePage extends StatelessWidget {
     final stream = locator.get<GetBrewingMethodsUseCase>().brewingMethods;
     return Scaffold(
       body: StreamBuilder<List<BrewingMethod>>(
-          stream: stream,
-          builder: (context, snapshot) {
-            final data = snapshot.data;
-            if (data != null) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: data.map((e) => Text(e.name)).toList(),
-              );
-            }
-            return const Center(child: Text('Home page'));
-          }),
+        stream: stream,
+        builder: (context, snapshot) {
+          final data = snapshot.data;
+          if (data != null) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: data.map((e) => Text(e.name)).toList(),
+            );
+          }
+          return Center(
+            child: Text(
+              'Home page',
+              style: context.theme.typo.title,
+            ),
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/design_system/title.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class Typo  {
+  TextStyle get title => const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontStyle: FontStyle.italic,
+  );
+  TextStyle get subtitle => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal
+  );
+}
+
