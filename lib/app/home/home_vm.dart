d9d0a808d86bf3ca7cbaaefe9e0d1c99c71dca8c
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-14 21:42:33 +1000

    feat: Create home page view model

---
+++ lib/app/home/home_vm.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:caffeio/app/mvvm/view_model.abs.dart';
+import 'package:caffeio/domain/use_cases/brewing_methods/fetch_brewing_methods_uc.dart';
+import 'package:caffeio/domain/use_cases/brewing_methods/get_brewing_methods_uc.dart';
+import 'package:caffeio/entities/brewing_methods/brewing_method.dart';
+import 'package:equatable/equatable.dart';
+import 'package:rxdart/rxdart.dart';
+
+class HomePageState extends Equatable {
+  final List<BrewingMethod> brewingMethods;
+
+  const HomePageState({
+    this.brewingMethods = const [],
+  });
+
+  HomePageState copyWith({
+    List<BrewingMethod>? brewingMethods,
+  }) {
+    return HomePageState(
+      brewingMethods: brewingMethods ?? this.brewingMethods,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        brewingMethods,
+      ];
+}
+
+class HomeViewModel extends ViewModel {
+  final FetchBrewingMethodsUseCase _fetchBrewingMethodsUseCase;
+  final GetBrewingMethodsUseCase _getBrewingMethodsUseCase;
+
+  final _subscriptions = CompositeSubscription();
+
+  final _state = BehaviorSubject<HomePageState>.seeded(const HomePageState());
+
+  Stream<HomePageState> get state => _state.stream;
+
+  HomeViewModel(
+    this._fetchBrewingMethodsUseCase,
+    this._getBrewingMethodsUseCase,
+  );
+
+  @override
+  Future<void> init() async {
+    await _fetchBrewingMethodsUseCase();
+    _subscriptions.add(_getBrewingMethodsUseCase().listen((methods) {
+      _state.add(_state.value.copyWith(brewingMethods: methods));
+    }));
+  }
+
+  @override
+  void dispose() {
+    _subscriptions.cancel();
+    _state.close();
+  }
+}
