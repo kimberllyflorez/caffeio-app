dc493473929842347bfc73e3612d3706588a5d28
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-14 21:41:54 +1000

    feat: Add view state object

---
+++ lib/app/mvvm/view_state.abs.dart
@@ -0,0 +1,66 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:caffeio/app/mvvm/view_model.abs.dart';
+import 'package:caffeio/app/router/route_spec.dart';
+import 'package:caffeio/app/di/di.dart';
+import 'package:flutter/material.dart';
+
+abstract class ViewState<T extends StatefulWidget, V extends ViewModel>
+    extends State<T> with AutoRouteAware {
+
+  late final V _viewModel;
+
+  V get viewModel => _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = locator.get<V>();
+    _viewModel.init();
+  }
+
+  void listenToNavigation(Stream<RouteSpec> stream) {
+    stream.listen((routeInfo) {
+      debugPrint(
+          'Navigation Action: ${routeInfo.action} route: ${routeInfo.routes}');
+      debugPrint('Current stack ${context.router.stack}');
+      switch (routeInfo.action) {
+        case RouteAction.push:
+          context.router.push(routeInfo.routes.first);
+          break;
+        case RouteAction.replaceAllWithOne:
+          context.router.replaceAll([routeInfo.routes.first]);
+          break;
+        case RouteAction.replaceAllWithMany:
+          context.router.replaceAll(routeInfo.routes);
+          break;
+        case RouteAction.pop:
+          context.router.pop();
+          break;
+        case RouteAction.popForced:
+          context.router.popForced();
+          break;
+        case RouteAction.popAndPush:
+          context.router.popAndPush(routeInfo.routes.first);
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example App'),
+      ),
+      body: const Center(
+        child: Text('Example Screen'),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+}
