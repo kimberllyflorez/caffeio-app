52022cda526804c042e961099f3b1f6abf9c7515
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-14 21:43:39 +1000

    chore: Move app router to main

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,19 +1,21 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:caffeio/di/di.dart';
-import 'package:caffeio/domain/use_cases/brewing_methods/fetch_brewing_methods_uc.dart';
-import 'package:caffeio/domain/use_cases/brewing_methods/get_brewing_methods_uc.dart';
-import 'package:caffeio/entities/brewing_methods/brewing_method.dart';
+import 'package:caffeio/app/home/home_vm.dart';
+import 'package:caffeio/app/mvvm/view_state.abs.dart';
 import 'package:caffeio/design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ViewState<HomePage, HomeViewModel> {
+  @override
   Widget build(BuildContext context) {
-    locator.get<FetchBrewingMethodsUseCase>().fetchBrewingMethods();
-    final stream = locator.get<GetBrewingMethodsUseCase>().brewingMethods;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -21,14 +23,14 @@ class HomePage extends StatelessWidget {
           style: context.theme.typo.title,
         ),
       ),
-      body: StreamBuilder<List<BrewingMethod>>(
-        stream: stream,
+      body: StreamBuilder<HomePageState>(
+        stream: viewModel.state,
         builder: (context, snapshot) {
           final data = snapshot.data;
           if (data != null) {
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: data.map((e) => Text(e.name)).toList(),
+              children: data.brewingMethods.map((e) => Text(e.name)).toList(),
             );
           }
           return Center(
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,21 @@
 import 'package:caffeio/app/router/app_router.dart';
-import 'package:caffeio/di/di.dart';
+import 'package:caffeio/app/di/di.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  final dependency = DependencyInjection();
-  await dependency.setup();
-  runApp(CaffeioApp(appRouter: locator.get<AppRouter>()));
+  await DependencyInjection.setup();
+  runApp(CaffeioApp());
 }
 
 class CaffeioApp extends StatelessWidget {
-  final AppRouter appRouter;
+  final AppRouter _appRouter = AppRouter();
 
-  const CaffeioApp({
-    super.key,
-    required this.appRouter,
-  });
+  CaffeioApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: appRouter.config(),
+      routerConfig: _appRouter.config(),
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
