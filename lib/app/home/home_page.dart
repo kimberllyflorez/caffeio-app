e09371e1ca65df52fd32932daa4cc0e60d4ff86a
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-15 21:45:33 +1000

    chore: Update navigation from home to Login

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -71,7 +71,7 @@ class _HomePageState extends ViewState<HomePage, HomeViewModel> {
                     visible: state.history.isNotEmpty,
                     replacement: Center(
                       child: CaffeioButton(
-                        callback: () {},
+                        callback: viewModel.onLoginPressed,
                         text: 'Login',
                       ),
                     ),
