892c42a8a5f2793d3ed285f049b6ea73e234c064
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-15 14:15:53 +1000

    chore: Update use cases with use case object

--- lib/domain/use_cases/brewing_methods/fetch_brewing_methods_uc.dart
+++ lib/domain/use_cases/brewing_methods/fetch_brewing_methods_uc.dart
@@ -1,9 +1,11 @@
 import 'package:caffeio/data/repositories/brewing_methods_repository.abs.dart';
+import 'package:caffeio/domain/use_cases.abs.dart';
 
-class FetchBrewingMethodsUseCase {
+class FetchBrewingMethodsUseCase extends NoParamsVoidUseCase {
   final BrewingMethodsRepository _repository;
 
   FetchBrewingMethodsUseCase(this._repository);
 
+  @override
   Future<void> call() => _repository.fetchBrewingMethods();
 }
--- lib/domain/use_cases/brewing_methods/get_brewing_methods_uc.dart
+++ lib/domain/use_cases/brewing_methods/get_brewing_methods_uc.dart
@@ -1,11 +1,14 @@
 import 'package:caffeio/data/repositories/brewing_methods_repository.abs.dart';
+import 'package:caffeio/domain/use_cases.abs.dart';
 import 'package:caffeio/entities/brewing_methods/brewing_method.dart';
 
-class GetBrewingMethodsUseCase {
+class GetBrewingMethodsUseCase
+    extends NoParamsStreamUseCase<List<BrewingMethod>> {
   final BrewingMethodsRepository _repository;
 
   GetBrewingMethodsUseCase(this._repository);
 
+  @override
   Stream<List<BrewingMethod>> call() {
     return _repository.brewingMethodsStream;
   }
