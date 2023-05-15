7c1c8e7c7682065e93a7161f775bae5ddd1e0f15
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-15 21:41:52 +1000

    feat: Add new view models to DI

--- lib/app/di/di_view_models.dart
+++ lib/app/di/di_view_models.dart
@@ -1,10 +1,18 @@
+import 'package:caffeio/app/auth/login/login_vm.dart';
 import 'package:caffeio/app/home/home_vm.dart';
+import 'package:caffeio/app/settings/settings_vm.dart';
 import 'package:get_it/get_it.dart';
 
 class DiViewModel {
   static Future<void> setUp(GetIt getIt) async {
     getIt.registerFactory<HomeViewModel>(() {
       return HomeViewModel(getIt.get(), getIt.get());
+    });
+    getIt.registerFactory<SettingsViewModel>(() {
+      return SettingsViewModel();
+    });
+    getIt.registerFactory<LoginViewModel>(() {
+      return LoginViewModel();
     });
   }
 }
