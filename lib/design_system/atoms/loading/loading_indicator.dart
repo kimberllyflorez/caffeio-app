3b13fea55d47b0b21da4b336a023add5223dc02c
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-15 11:02:43 +1000

    feat: Add loading indicator widget

---
+++ lib/design_system/atoms/loading/loading_indicator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(
+        color: Colors.red,
+      ),
+    );
+  }
+}
