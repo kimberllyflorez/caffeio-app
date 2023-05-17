a59d51f7f77ef2b5099010ec6c92927d8d522ce4
kimberlly florez <kimberlly.florez@gmail.com>
2023-05-17 14:32:40 +1000

    Feat: structure of recommendation section

--- lib/app/brew/recommendations/recommendations_page.dart
+++ lib/app/brew/recommendations/recommendations_page.dart
@@ -1,5 +1,5 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:caffeio/app/brew/recommendations_vm.dart';
+import 'package:caffeio/app/brew/recommendations/recommendations_vm.dart';
 import 'package:caffeio/app/mvvm/view_state.abs.dart';
 import 'package:caffeio/design_system/atoms/buttons/caffeio_button.dart';
 import 'package:flutter/material.dart';
