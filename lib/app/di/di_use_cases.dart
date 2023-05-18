7b55c17d92ad4cb29d050f632f636cb5f9699439
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-18 20:47:48 +1000

    chore: Update DI

--- lib/app/di/di_use_cases.dart
+++ lib/app/di/di_use_cases.dart
@@ -1,18 +1,18 @@
 import 'package:caffeio/domain/use_cases/brewing_methods/fetch_brewing_methods_uc.dart';
 import 'package:caffeio/domain/use_cases/brewing_methods/get_brewing_methods_uc.dart';
-import 'package:caffeio/domain/use_cases/ratio/ratio_calculate_water.dart';
-import 'package:caffeio/domain/use_cases/timer/timer_formated_use_case.dart';
+import 'package:caffeio/domain/use_cases/brewing_methods/get_user_brew_uc.dart';
+import 'package:caffeio/domain/use_cases/brewing_methods/set_user_brew_uc.dart';
+import 'package:caffeio/domain/use_cases/ratio/caculate_water_ratio_uc.dart';
+import 'package:caffeio/domain/use_cases/timer/format_stopwatch_time_uc.dart';
 import 'package:get_it/get_it.dart';
 
 class DiUseCase {
   static Future<void> setUp(GetIt getIt) async {
-    getIt.registerFactory<FetchBrewingMethodsUseCase>(
-        () => FetchBrewingMethodsUseCase(getIt.get()));
-    getIt.registerFactory<GetBrewingMethodsUseCase>(
-        () => GetBrewingMethodsUseCase(getIt.get()));
-    getIt.registerFactory<TimerFormatUseCase>(
-      () => TimerFormatUseCase(),
-    );
-    getIt.registerFactory<RatioCalculateWater>(() => RatioCalculateWater());
+    getIt.registerFactory(() => FetchBrewingMethodsUseCase(getIt.get()));
+    getIt.registerFactory(() => GetBrewingMethodsUseCase(getIt.get()));
+    getIt.registerFactory(() => FormatStopwatchTimeUseCase());
+    getIt.registerFactory(() => CalculateWaterRatioUseCase());
+    getIt.registerFactory(() => GetUserBrewUseCase(getIt.get()));
+    getIt.registerFactory(() => SetUserBrewUseCase(getIt.get()));
   }
 }
