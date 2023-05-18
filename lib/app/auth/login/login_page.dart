c149d2e26a2ae02efe75721d8b80bf46ebe6fcf1
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-18 22:54:30 +1000

    feat: Add login functionality

--- lib/app/auth/login/login_page.dart
+++ lib/app/auth/login/login_page.dart
@@ -1,11 +1,25 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:caffeio/app/auth/login/login_page_vm.dart';
+import 'package:caffeio/app/mvvm/view_state.abs.dart';
 import 'package:caffeio/design_system/atoms/buttons/caffeio_button.dart';
+import 'package:caffeio/design_system/atoms/loading/loading_indicator.dart';
 import 'package:caffeio/design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ViewState<LoginPage, LoginPageViewModel> {
+  @override
+  void initState() {
+    super.initState();
+    listenToNavigation(viewModel.router);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,37 +28,48 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: theme.spacing.s),
-            Padding(
-              padding: theme.insets.s.toHorizontal,
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'email',
+      body: StreamBuilder<LoginPageState>(
+        stream: viewModel.state,
+        builder: (context, snapshot) {
+          final state = snapshot.data;
+          if (state == null) {
+            return const LoadingIndicator();
+          }
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: theme.spacing.s),
+                Padding(
+                  padding: theme.insets.s.toHorizontal,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'email',
+                    ),
+                    onChanged: viewModel.onChangeEmail,
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: theme.spacing.s),
-            Padding(
-              padding: theme.insets.s.toHorizontal,
-              child: const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
+                SizedBox(height: theme.spacing.s),
+                Padding(
+                  padding: theme.insets.s.toHorizontal,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                    onChanged: viewModel.onChangePassword,
+                  ),
                 ),
-              ),
+                SizedBox(height: theme.spacing.s),
+                CaffeioButton(
+                  callback: viewModel.onLogin,
+                  text: 'Log in',
+                )
+              ],
             ),
-            SizedBox(height: theme.spacing.s),
-            CaffeioButton(
-              callback: () {},
-              text: 'Log in',
-            )
-          ],
-        ),
+          );
+        },
       ),
     );
   }
