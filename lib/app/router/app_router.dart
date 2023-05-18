6832322a9c759a0e2ff4bbea0d9a4846a0d97881
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-18 20:47:39 +1000

    chore: Set first user brew guard

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,12 +1,21 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:caffeio/app/router/app_router.gr.dart';
+import 'package:caffeio/app/router/guards/first_user_brew_guard.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends $AppRouter {
+  final FirstUserBrewGuard _firstUserBrewGuard;
+
+  AppRouter(this._firstUserBrewGuard);
+
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: HomeRoute.page),
-        AutoRoute(page: MethodSelectionRoute.page, initial: true),
+        AutoRoute(
+          page: HomeRoute.page,
+          guards: [_firstUserBrewGuard],
+          initial: true,
+        ),
+        AutoRoute(page: MethodSelectionRoute.page),
         AutoRoute(page: RatioRoute.page),
         AutoRoute(page: RecommendationsRoute.page),
         AutoRoute(page: MethodRoute.page),
---
+++ lib/app/router/guards/first_user_brew_guard.dart
@@ -0,0 +1,22 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:caffeio/app/router/app_router.gr.dart';
+import 'package:caffeio/domain/use_cases/brewing_methods/get_user_brew_uc.dart';
+
+class FirstUserBrewGuard extends AutoRouteGuard {
+  final GetUserBrewUseCase _getUserBrewUseCase;
+
+  FirstUserBrewGuard(this._getUserBrewUseCase);
+
+  @override
+  Future<void> onNavigation(
+    NavigationResolver resolver,
+    StackRouter router,
+  ) async {
+    final userBrew = _getUserBrewUseCase();
+    if (userBrew != null) {
+      resolver.next(true);
+    } else {
+      router.replaceAll([const MethodSelectionRoute()]);
+    }
+  }
+}
