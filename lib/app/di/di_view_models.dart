a47a44bba0ccf021cd4f4b1000c35ba44974d1aa
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-18 22:54:55 +1000

    chore: Update DI

--- lib/app/di/di_view_models.dart
+++ lib/app/di/di_view_models.dart
@@ -1,7 +1,7 @@
 import 'package:caffeio/app/brew/methods_selection/method_selection_vm.dart';
 import 'package:caffeio/app/brew/ratio/ratio_vm.dart';
 import 'package:caffeio/app/brew/recommendations/recommendations_vm.dart';
-import 'package:caffeio/app/auth/login/login_vm.dart';
+import 'package:caffeio/app/auth/login/login_page_vm.dart';
 import 'package:caffeio/app/home/home_vm.dart';
 import 'package:caffeio/app/settings/settings_vm.dart';
 import 'package:get_it/get_it.dart';
@@ -10,9 +10,15 @@ import '../brew/timer/timer_vm.dart';
 
 class DiViewModel {
   static Future<void> setUp(GetIt getIt) async {
-    getIt.registerFactory(() => HomeViewModel(getIt.get(), getIt.get()));
-    getIt.registerFactory(() => SettingsViewModel());
-    getIt.registerFactory(() => LoginViewModel());
+    getIt.registerFactory(() {
+      return HomeViewModel(
+        getIt.get(),
+        getIt.get(),
+        getIt.get(),
+      );
+    });
+    getIt.registerFactory(() => SettingsViewModel(getIt.get(), getIt.get()));
+    getIt.registerFactory(() => LoginPageViewModel(getIt.get()));
     getIt.registerFactory(() => MethodSelectionViewModel(getIt.get()));
     getIt.registerFactory(() => RatioViewModel(getIt.get()));
     getIt.registerFactory(() => RecommendationViewModel());
