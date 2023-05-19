8ebdbefd6e2e9fa1c000c13c78182ff9796dd75c
kimberlly florez <kimberlly.florez@gmail.com>
2023-05-19 18:31:39 +1000

    style: ratio page

--- lib/app/brew/ratio/ratio_page.dart
+++ lib/app/brew/ratio/ratio_page.dart
@@ -3,6 +3,7 @@ import 'package:caffeio/app/brew/ratio/ratio_vm.dart';
 import 'package:caffeio/app/mvvm/view_state.abs.dart';
 import 'package:caffeio/design_system/atoms/buttons/caffeio_button.dart';
 import 'package:caffeio/design_system/atoms/loading/loading_indicator.dart';
+import 'package:caffeio/design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -25,57 +26,70 @@ class _RatioPageState extends ViewState<RatioPage, RatioViewModel> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: Text(
+          'Coffee Ratio',
+          style: context.theme.typo.title,
+        ),
       ),
       body: StreamBuilder<RatioState>(
         stream: viewModel.state,
         builder: (context, snapshot) {
           final data = snapshot.data;
           if (data != null) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text('coffee amount'),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Colors.purple,
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: context.theme.insets.xs.toHorizontal,
+                    child: Text(
+                        'How many grams of coffee do you want to prepared?',
+                        style: context.theme.typo.subtitle),
+                  ),
+                  SizedBox(height: context.theme.spacing.m),
+                  Padding(
+                    padding: context.theme.insets.xs.toHorizontal,
                     child: TextFormField(
                       onChanged: (grams) => viewModel.saveGramsCoffee(grams),
                       decoration: const InputDecoration(
-                        hintText: "20",
-                      ),
+                          hintText: "20", suffixText: "gr"),
                       textAlign: TextAlign.center,
                       inputFormatters: [LengthLimitingTextInputFormatter(3)],
                     ),
                   ),
-                ),
-                const Text("Ratio"),
-                Column(
-                  children: [
-                    Text("1: ${data.ratioModel.ratio.toString()}"),
-                    Slider(
-                      divisions: 25,
-                      min: 10,
-                      max: 25,
-                      label: "1:${data.ratioModel.ratio.toString()}",
-                      value: data.ratio,
-                      onChanged: (value) => viewModel.saveRatio(value),
+                  SizedBox(height: context.theme.spacing.m),
+                  Padding(
+                    padding: context.theme.insets.xs.toHorizontal,
+                    child: Text(
+                      "Ratio:  1: ${data.ratioModel.ratio.toString()}",
+                      style: context.theme.typo.subtitle,
                     ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      const Text("Total water"),
-                      Text(viewModel.totalWater()),
-                    ],
                   ),
-                )
-              ],
+                  SizedBox(height: context.theme.spacing.m),
+                  Slider(
+                    divisions: 10,
+                    min: 10,
+                    max: 25,
+                    label: "1:${data.ratioModel.ratio.toString()}",
+                    value: data.ratio,
+                    onChanged: (value) => viewModel.saveRatio(value),
+                  ),
+                  Padding(
+                    padding: context.theme.insets.xs.toHorizontal,
+                    child: Row(
+                      children: [
+                        Text(
+                          viewModel.totalWater(),
+                          style: context.theme.typo.title.copyWith(
+                            color: context.theme.palette.blueScale.primaryColor,
+                          ),
+                        ),
+                        Text(" of water do you need",
+                            style: context.theme.typo.subtitle),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             );
           }
           return const LoadingIndicator();
