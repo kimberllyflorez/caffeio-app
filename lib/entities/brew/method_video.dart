f36ac5d6f0ed18069a9ea0ec5058448684902ea0
kimberlly florez <kimberlly.florez@gmail.com>
2023-05-19 16:47:06 +1000

    created model of methods videos

---
+++ lib/entities/brew/method_video.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'method_video.g.dart';
+
+@JsonSerializable(createToJson: false)
+class MethodVideo extends Equatable {
+  final int id;
+  @JsonKey(name: 'method_id')
+  final int methodId;
+  final String url;
+
+  const MethodVideo( {
+    required this.id,
+    required this.methodId,
+    required this.url,
+  });
+
+  factory MethodVideo.fromJson(Map<String, dynamic> json) =>
+      _$MethodVideoFromJson(json);
+
+  @override
+  List<Object?> get props => [id, methodId, url];
+}
