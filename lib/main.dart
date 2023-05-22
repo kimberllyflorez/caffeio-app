785f2cddfb7cabab9f063c061a842cc55b4186c6
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-22 17:14:52 +1000

    style: Format code

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:caffeio/design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-
   await DependencyInjection.setup();
   final appRouter = AppRouter(locator.get<FirstUserBrewGuard>());
   runApp(CaffeioApp(appRouter: appRouter));
