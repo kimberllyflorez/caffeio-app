c46f608d1a39663cdf6d73d63aaeea6d3142472f
Cesar Urrego <caurregoz.is@gmail.com>
2023-05-22 13:40:28 +1000

    chore: Add get brew by date use case

---
+++ lib/domain/use_cases/brew/get_brews_by_date_uc.dart
@@ -0,0 +1,39 @@
+import 'package:caffeio/app/brew/ratio/ratio_model.dart';
+import 'package:caffeio/app/home/model/brew_by_date.dart';
+import 'package:caffeio/domain/use_cases.abs.dart';
+import 'package:intl/intl.dart';
+
+class GetBrewsByDateUseCase
+    extends UseCase<List<BrewByDate>, List<RatioModelView>> {
+
+  //TODO: Improve logic
+  @override
+  List<BrewByDate> call(List<RatioModelView> params) {
+    Map<String, List<RatioModelView>> groupedMethods = {};
+    Map<String, double> totalGrams = {};
+
+    for (var brew in params) {
+      final String date = DateFormat('dd/MM/yyyy').format(
+        brew.creationDate ?? DateTime.now(),
+      );
+      double grams = brew.gramsCoffee;
+
+      if (!groupedMethods.containsKey(date)) {
+        groupedMethods[date] = [];
+        totalGrams[date] = 0;
+      }
+
+      groupedMethods[date]!.add(brew);
+      totalGrams[date] = (totalGrams[date] ?? 0) + grams;
+    }
+
+    List<BrewByDate> result = [];
+
+    groupedMethods.forEach((date, methods) {
+      double total = totalGrams[date]!;
+      result.add(BrewByDate(date, methods, total));
+    });
+
+    return result;
+  }
+}
