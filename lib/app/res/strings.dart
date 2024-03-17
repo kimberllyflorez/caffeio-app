272471e8083f5618ec25802028bb24e84898be59
Cesar Urrego <caurregoz.is@gmail.com>
2024-03-17 12:01:20 +1100

    chore: Add resources files

---
+++ lib/app/res/strings.dart
@@ -0,0 +1,12 @@
+class CaffeioStrings {
+  static const String appTitle = 'Caffeio App';
+  static const String appVersion = 'v1.0.0';
+
+  /// Brand
+  static const String brandName = 'Bimbly Studios';
+
+  /// Support
+  static const String loginButtonLabel = 'Login';
+  static const String supportButtonLabel = 'Support';
+  static const String logoutButtonLabel = 'Log out';
+}
